bb7c0ab1814bde56d3e9f4182b5e4056dfe2e3aa
samue <samue@10.5.22.252>
2016-05-06 19:12:43 -0300

    jokenpo codigo cliente; algumas atualizações, agora funciona kkkk

---
+++ Documents/NetBeansProjects/PalitinhosClient/src/Connector.java
@@ -0,0 +1,47 @@
+import java.net.MalformedURLException;
+import java.rmi.NotBoundException;
+import java.rmi.RemoteException;
+ 
+public class Connector {
+    private RMIClient client;
+    private Connect connect;
+     
+    private void connect() {
+        try {
+            client = new RMIClient("localhost");
+            connect = (Connect) client.getRemoteObject();
+        } catch (MalformedURLException e) {
+            e.printStackTrace();
+        } catch (RemoteException e) {
+            e.printStackTrace();
+        } catch (NotBoundException e) {
+            e.printStackTrace();
+        }
+    }
+		
+    public Connect getConnect() {
+    	return connect;
+    }
+    
+    public static void main(String[] args) {
+        Connector connector = new Connector();
+         
+        connector.connect();
+         
+        Connect obj = connector.getConnect();
+         
+        try {
+            
+            obj.jogar(0);
+            obj.login("Samuel");
+            obj.jogar(4);
+            obj.login("Reinaldo");
+ 
+            for (String s : obj.getLista()) {
+                System.out.println(s);
+                }
+        } catch (RemoteException e) {
+            e.printStackTrace();
+        }
+    }
+}
